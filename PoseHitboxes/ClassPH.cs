b33f6dd9c07b483700e34f72f55e51b560902210
matthew <matthew@DESKTOP-JEVN8QL>
2024-04-20 11:08:53 -0500

    Add project files.

---
+++ PoseHitboxes/ClassPH.cs
@@ -0,0 +1,100 @@
+﻿using MelonLoader;
+using RUMBLE.Tutorial.MoveLearning;
+using RumbleModdingAPI;
+using static RumbleModdingAPI.Calls;
+using UnityEngine;
+using static RUMBLE.Players.Subsystems.PlayerVR;
+
+
+namespace PoseHitboxes
+{
+    public class ClassPH : MelonMod
+    {
+        private string currentScene = "Loader";
+        private bool sceneChanged = false;
+        private bool loaded = false;
+        PoseGhost poseGhost;
+        Material ghosty;
+        Material redy;
+        string currentPoseName = "";
+        int delayCycle = 0;
+
+    public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            currentScene = sceneName;
+            sceneChanged = true;
+            delayCycle = 180;
+            //MelonLogger.Msg("delay frames:");
+            //MelonLogger.Msg(delayCycle);
+        }
+
+        public override void OnUpdate()
+        {
+            if (currentScene != "Gym") { return; }
+            if (sceneChanged)
+            {
+                if (delayCycle > 1)
+                {
+                    delayCycle--; // wait 180 frames before trying to fetch anything
+                    //MelonLogger.Msg(delayCycle);
+                }
+                else
+                {
+                    if (!loaded)
+                    {
+                        try
+                        {
+                            //MelonLogger.Msg("loading items");
+                            poseGhost = Calls.GameObjects.Gym.Logic.HeinhouserProducts.MoveLearning.GetGameObject().GetComponentInChildren<PoseGhost>();//GameObject.Find("--------------LOGIC--------------/Heinhouser products/MoveLearning/Ghost").GetComponent<PoseGhost>();
+                            ghosty = poseGhost.transform.GetChild(0).GetChild(1).GetComponent<Renderer>().material;
+                            redy = Calls.GameObjects.Gym.Scene.GymProduction.MainStaticGroup.GymArena.GetGameObject().transform.GetChild(0).GetComponent<Renderer>().material;
+
+                            loaded = true;
+                            //MelonLogger.Msg("items loaded");
+                        }
+                        catch { return; }
+                    }
+                    sceneChanged = false;
+                }
+            }
+            if (loaded)
+            {
+
+                //if pose switched
+                if (currentPoseName != poseGhost.showCurrentPoseData.name)
+                {
+                    
+                    if (poseGhost.transform.GetChild(0).GetChild(0).childCount > 0)
+                    {
+                       // MelonLogger.Msg("Destroying Existing");
+                        while (poseGhost.transform.GetChild(0).GetChild(0).childCount > 0 || poseGhost.transform.GetChild(0).GetChild(1).childCount > 0)
+                        {
+                            GameObject.DestroyImmediate(poseGhost.transform.GetChild(0).GetChild(0).GetChild(0).gameObject);
+                            GameObject.DestroyImmediate(poseGhost.transform.GetChild(0).GetChild(1).GetChild(0).gameObject);
+                        }
+                    }
+                   // MelonLogger.Msg("Creating Primitives");
+                    GameObject LCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    LCube.transform.parent = poseGhost.transform.GetChild(0).GetChild(0);
+                    //LCube.transform.position = poseGhost.transform.GetChild(0).GetChild(1).position;
+                    LCube.transform.position = poseGhost.leftHand.position;//showCurrentPoseData.leftControllerCondition.DesiredPose.position;
+                    LCube.transform.localRotation = poseGhost.showCurrentPoseData.leftControllerCondition.DesiredPose.rotation;
+                    LCube.transform.localScale = new Vector3(poseGhost.showCurrentPoseData.leftControllerCondition.PositionTreshold, poseGhost.showCurrentPoseData.leftControllerCondition.PositionTreshold, poseGhost.showCurrentPoseData.leftControllerCondition.PositionTreshold);
+                    LCube.GetComponent<Renderer>().material = ghosty;
+                    poseGhost.transform.GetChild(0).GetChild(0).GetComponent<Renderer>().material = redy;
+
+                    //poseGhost.transform.GetChild(0).GetChild(0).localScale = new Vector3(poseGhost.showCurrentPoseData.LeftControllerCondition.PositionTreshold, poseGhost.showCurrentPoseData.LeftControllerCondition.PositionTreshold, poseGhost.showCurrentPoseData.LeftControllerCondition.PositionTreshold);
+                    GameObject RCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    RCube.transform.parent = poseGhost.transform.GetChild(0).GetChild(1);
+                    //RCube.transform.position = poseGhost.transform.position;
+                    RCube.transform.position = poseGhost.rightHand.position;
+                    RCube.transform.localRotation = poseGhost.showCurrentPoseData.rightControllerCondition.DesiredPose.rotation;
+                    RCube.transform.localScale = new Vector3(poseGhost.showCurrentPoseData.RightControllerCondition.PositionTreshold, poseGhost.showCurrentPoseData.RightControllerCondition.PositionTreshold, poseGhost.showCurrentPoseData.RightControllerCondition.PositionTreshold);
+                    RCube.GetComponent<Renderer>().material = ghosty;
+                    poseGhost.transform.GetChild(0).GetChild(1).GetComponent<Renderer>().material = redy;
+                    currentPoseName = poseGhost.showCurrentPoseData.name;
+                }
+            }
+        }
+    }
+}
---
+++ PoseHitboxes/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using MelonLoader;
+using PoseHitboxes;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: MelonInfo(typeof(ClassPH), "PoseHitboxes", "1.0.0", "Marcopolo1613")]
+[assembly: MelonGame("Buckethead Entertainment", "RUMBLE")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PoseHitboxes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PoseHitboxes")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("138ef4a5-245f-4c7b-a130-69645d5b6e5b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
